058dfb2f7956ce5b058520d9a0e7b5c23fa928cc
Armin <armin.bagheri.1380@gmail.com>
2016-05-19 23:19:31 +0430

    Update and rename groupcontrol.lua to SuperGroup.lua

--- plugins/SuperGroup.lua
+++ plugins/SuperGroup.lua
@@ -1,8 +1,8 @@
 do
 
 -- make sure to set with value that not higher than stats.lua
-local NUM_MSG_MAX = 4
-local TIME_CHECK = 4 -- seconds
+local NUM_MSG_MAX = 2
+local TIME_CHECK = 100 
 
 local function generate_link(cb_extra, success, result)
     local receiver = cb_extra.receiver
